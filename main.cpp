b36904e490f7cee79c63eb528c535dfa739f21b8
lokeshkumaraguru <lkumaraguru@mail.csuchico.edu>
2018-10-01 16:10:12 -0700

    main.cpp

--- main.cpp
+++ main.cpp
@@ -47,6 +47,12 @@ while (input != 'q' || input != 'Q')
   if(input == 'q' || input == 'Q')
   {
 
+  cout<<"==ITEMS TO BUY=="<<endl;
+  cout<<"1 "<<list[0]<<endl;
+  cout<<"2 "<<list[1]<<endl;
+  cout<<"3 "<<list[2]<<endl;
+  cout<<"4 "<<list[3]<<endl;
+  cout<<"5 "<<list[4]<<endl;
   break;
 
   }
