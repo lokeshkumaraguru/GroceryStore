1d99b777e6a40c8c94e47f94bb09616559675f7c
lokeshkumaraguru <lkumaraguru@mail.csuchico.edu>
2018-10-10 16:42:57 -0700

    main.cpp

--- main.cpp
+++ main.cpp
@@ -14,56 +14,86 @@ int numItems = 0;
 char input;
 string itemone;
 
-while(input != 'q' &&  input != 'Q')
-
+while(input != 'Q' && input != 'q')
 {
 
+  if(numItems == 0)
+  {
+  
   cout<<"\n==GROCERY LIST MANAGER==";
   cout<<"\nEnter your choice: ";
   cout<<"\n (A)dd an item";
   cout<<"\n (Q)uit";
   cout<<"\nYour choice (A/Q): ";
   cin>>input;
-
-  if(input == 'a' || input == 'A')
   
+  }
+
+  else
   {
+  
+  cout<<"\n==GROCERY LIST MANAGER==";
+  cout<<"\nEnter your choice: ";
+  cout<<"\n (A)dd an item";
+  cout<<"\n (D)elete last item";
+  cout<<"\n (Q)uit";
+  cout<<"\nYour choice (A/D/Q): ";
+  cin>>input;
+  
+  }
+
+  if(input == 'A' || input == 'a')
+  {
+  
+  cout<<"What is the item?"<<endl;
+  cin>>itemone;
+
+  list.push_back(itemone);
+  numItems = numItems + 1;
+  
+  }
+
+  if(input == 'Q' || input =='q')
+  {
+  
+  if(numItems > 0)
+  {
+  
+    cout<<"==ITEMS TO BUY=="<<endl;
+    for(int index = 0; index < numItems; index++)
+    {
     
-    cout<<"What is the item?"<<endl;
-    cin>>itemone;
-
-    list.push_back(itemone);
-    numItems = numItems + 1;
+      cout<<index+1<<" "<<list[index]<<endl;
+    
+    }
 
   }
 
-}
-
-if(input == 'q' || input == 'Q')
-
-{
-if(numItems > 0)
-  {
-  cout<<"==ITEMS TO BUY=="<<endl;
-  for (int index = 0;  index < numItems; index++)
-   
-  {
-
-  cout<<index+1<<" "<<list[index]<<endl;
-
-  }
-  }
-
-else
+  else
   {
   
   cout<<"No items to buy!"<<endl;
   
   }
 
+  }
+
+  if(input == 'D' || input == 'd')
+  {
+  
+  if(numItems > 0)
+  {
+  
+    cout<<itemone;
+    cout<<" was deleted from the list.\n"<<endl;
+    list.pop_back();
+    numItems = numItems - 1;
+  
+  }
+  
+  }
+
 }
 
 return 0;
-
 }
-
